8f4527bf8214d44a9237d01dd664d626d3f5d303
Guillaume Pouilloux <gui.pouilloux@gmail.com>
2017-01-17 17:27:49 +0100

    [elasticsearch] use suggest api rather than a simple search query for performance concerns

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/ElasticSearchRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/ElasticSearchRepository.java
@@ -6,6 +6,8 @@ import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.HttpClientConfig;
 import io.searchbox.core.Search;
 import io.searchbox.core.SearchResult;
+import io.searchbox.core.Suggest;
+import io.searchbox.core.SuggestResult;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -32,24 +34,26 @@ public class ElasticSearchRepository {
         return jestClient;
     }
 
-    public List<String> getActorsSuggests(String searchQuery) {
+    public List<String> getActorsSuggests(String suggestQuery) {
         String query = "{\n" +
-                "    \"size\": 5,\n" +
-                "    \"query\": {\n" +
-                "            \"match\" : {\n" +
-                "                    \"name\" : \"*" + searchQuery + "*\"\n" +
-                "            }\n" +
-                "    }\n" +
-                "}";
+                "\"actor-suggestion\": {\n" +
+	                "    \"text\":\"" + suggestQuery + "\"\n" +
+		            "    \"term\": {\n" +
+		            "       \"field\": \"name\"\n" +
+		            "       }\n" +
+	                "    }\n" +
+                    "}" +
+		        "}";
 
-        Search search = new Search.Builder(query)
-                .addIndex("movies")
-                .addType("actor")
-                .build();
+	    Suggest suggest = new Suggest.Builder(query)
+			    .addIndex("movies")
+			    .addType("actor")
+			    .build();
 
-        ArrayList<String> list = new ArrayList<>();
+
+	    ArrayList<String> list = new ArrayList<>();
         try {
-            SearchResult result = jestClient.execute(search);
+            SuggestResult result = jestClient.execute(suggest);
             result.getJsonObject().get("hits").getAsJsonObject().get("hits")
                     .getAsJsonArray().forEach(jsonElement -> {
                 list.add(jsonElement.getAsJsonObject().get("_source").getAsJsonObject().get("name").getAsString());
