34aa7fcc3f788d22e2d2561fb32ace6942c74445
Arthur <arthur.faugeras@gmail.com>
2017-01-17 10:48:34 +0100

    Début sur la suite du processus relatif à elasticsearch

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/ElasticSearchRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/ElasticSearchRepository.java
@@ -3,7 +3,11 @@ package com.serli.oracle.of.bacon.repository;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ElasticSearchRepository {
@@ -27,9 +31,27 @@ public class ElasticSearchRepository {
         return jestClient;
     }
 
-    public List<String> getActorsSuggests(String searchQuery) {
-        // TODO implent suggest
-        return null;
+    public List<String> getActorsSuggests(String searchQuery) throws IOException {
+        String query = "{\n" +
+                "    \"query\": {\n" +
+                "            \"match\" : {\n" +
+                "                    \"name\" : \"*evi*\"\n" +
+                "            },\n" +
+                "    }\n" +
+                "}";
+
+        Search search = new Search.Builder(query)
+                .addIndex("movies")
+                .addType("actor")
+                .build();
+
+        SearchResult result = jestClient.execute(search);
+        List<SearchResult.Hit<String, Void>> hits = result.getHits(String.class);
+        ArrayList<String> list = new ArrayList<>();
+        hits.forEach(element -> {
+            
+        });
+        return list;
     }
 
 
