4d3fd0da8663c1963250c3203a8c090963efbded
Guillaume Pouilloux <gui.pouilloux@gmail.com>
2017-01-17 08:54:40 +0100

    [redis] implement last 10 actor searches

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/RedisRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/RedisRepository.java
@@ -1,10 +1,24 @@
 package com.serli.oracle.of.bacon.repository;
 
+import redis.clients.jedis.Jedis;
+
 import java.util.List;
 
 public class RedisRepository {
+    private final Jedis driver;
+
+    private final String SEARCH_ACTOR_KEY = "searches_actor";
+
+    public RedisRepository() {
+        driver = new Jedis("localhost", 6379);
+    }
+
     public List<String> getLastTenSearches() {
-        // TODO implement last 10 searchs
-        return null;
+        // implement last 10 searchs
+        return driver.lrange(SEARCH_ACTOR_KEY, 0, 9);
+    }
+
+    public void appendActorSearch(String searchActor) {
+        driver.lpush(SEARCH_ACTOR_KEY, searchActor);
     }
 }
